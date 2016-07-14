8dc0f9404d987f417089a275a65ff985a34d9736
sqfasd <sqf1225@foxmail.com>
2016-07-14 11:14:32 +0800

    fix upvote options issue

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var package = require('./package.json');
 function main() {
 	program.version(package.version)
 		.option('-H, --host <host>', 'Specify the hostname or ip of the node, default: 127.0.0.1')
-		.option('-p, --port <port>', 'Specify the port of the node, default: 4096')
+		.option('-P, --port <port>', 'Specify the port of the node, default: 4096')
 	
 	var plugins = fs.readdirSync(path.join(__dirname, 'plugins'));
 	plugins.forEach(function (el) {
