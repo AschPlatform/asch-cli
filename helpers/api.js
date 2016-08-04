746d78eef607dd3057f920aa396fd282aa17f6b6
sqfasd <sqf1225@foxmail.com>
2016-08-04 23:50:58 +0800

    update magic

--- helpers/api.js
+++ helpers/api.js
@@ -65,7 +65,7 @@ Api.prototype.broadcastTransaction = function (trs, cb) {
     url: this.baseUrl + "/peer/transactions",
     // TODO magic should be read from a config file or options
     headers: {
-      magic: "15a3b1d6",
+      magic: "594fe0f3",
       version: ""
     },
     json: {
