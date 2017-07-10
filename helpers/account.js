a22198e069da8a07a754d79e0da84333e5c65754
sqfasd <sqf1225@foxmail.com>
2017-07-10 09:13:11 +0800

    fix generate address issue

--- helpers/account.js
+++ helpers/account.js
@@ -3,7 +3,7 @@ var crypto = require('../lib/crypto.js');
 module.exports = {
 	account: function (secret) {
 		var kp = crypto.keypair(secret);
-		var address = crypto.getId(new Buffer(kp.publicKey, 'hex'));
+		var address = crypto.getAddress(new Buffer(kp.publicKey, 'hex'));
 
 		return {
 			keypair: kp,
